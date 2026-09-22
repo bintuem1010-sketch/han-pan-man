dc2a657d0a5c0b5df5286f2e80db18d51a065073
bintuem1010-sketch <bintuem1010@gmail.com>
2026-09-22 15:11:44 +0900

    Improve onboarding and rule discovery in sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "han-pan-man-v1";
+const CACHE = "han-pan-man-v2";
 const ASSETS = ["./", "index.html", "style.css", "game.js", "manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
